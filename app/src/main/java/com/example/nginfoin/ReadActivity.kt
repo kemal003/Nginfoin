980ced18778f19b1fa93692342c5443ce92c411c
ansharfz <ansharfarizzulhilmi@gmail.com>
2021-11-28 13:46:17 +0700

    update baca article

---
+++ app/src/main/java/com/example/nginfoin/ReadActivity.kt
@@ -0,0 +1,24 @@
+package com.example.nginfoin
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+
+class ReadActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read)
+        val toolbar : Toolbar = findViewById<Toolbar>(R.id.appbar_read)
+        setSupportActionBar(toolbar)
+        val article_title : TextView = findViewById<TextView>(R.id.article_title)
+        val article_content : TextView = findViewById<TextView>(R.id.article_content)
+        val title = intent.getStringExtra("title")
+        val content = intent.getStringExtra("content")
+        article_title.text = title
+        article_content.text = title
+
+    }
+}
