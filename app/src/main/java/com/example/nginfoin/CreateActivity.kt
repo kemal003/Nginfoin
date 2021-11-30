d7e8d289682d1534ad9d6015e175506e291e3b04
Andika Irza Pradana <andikairzapradana@gmail.com>
2021-11-30 22:29:03 +0800

    add button back create

--- app/src/main/java/com/example/nginfoin/CreateActivity.kt
+++ app/src/main/java/com/example/nginfoin/CreateActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 class CreateActivity : AppCompatActivity() {
@@ -15,6 +16,10 @@ class CreateActivity : AppCompatActivity() {
         val btn_create = include_create.findViewById<Button>(R.id.button_create)
         btn_create.setOnClickListener {
             Toast.makeText(this, "Artikel telah dibuat", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, ListActivity::class.java))
+        }
+        val btn_back = findViewById<ImageView>(R.id.back_create)
+        btn_back.setOnClickListener {
             startActivity(Intent(this, ListActivity::class.java))
         }
     }
