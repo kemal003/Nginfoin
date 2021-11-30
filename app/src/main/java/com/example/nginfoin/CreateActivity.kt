6583940dc4bcfee6044b4f5ec59523cd51bdc7e0
Andika Irza Pradana <andikairzapradana@gmail.com>
2021-11-30 22:09:56 +0800

    add button create

--- app/src/main/java/com/example/nginfoin/CreateActivity.kt
+++ app/src/main/java/com/example/nginfoin/CreateActivity.kt
@@ -1,11 +1,21 @@
 package com.example.nginfoin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class CreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+        val include_create = findViewById<View>(R.id.include_appbar_create)
+        val btn_create = include_create.findViewById<Button>(R.id.button_create)
+        btn_create.setOnClickListener {
+            Toast.makeText(this, "Artikel telah dibuat", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, ListActivity::class.java))
+        }
     }
 }
