f66507c3518ffcaf05cdaeabb9382fb8f328481f
Andika Irza Pradana <andikairzapradana@gmail.com>
2021-11-30 22:10:14 +0800

    add button edit

--- app/src/main/java/com/example/nginfoin/EditActivity.kt
+++ app/src/main/java/com/example/nginfoin/EditActivity.kt
@@ -1,11 +1,21 @@
 package com.example.nginfoin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+        val include_edit = findViewById<View>(R.id.include_appbar_edit)
+        val btn_edit = include_edit.findViewById<Button>(R.id.button_edit)
+        btn_edit.setOnClickListener {
+            Toast.makeText(this, "Artikel telah diedit", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, ListActivity::class.java))
+        }
     }
 }
