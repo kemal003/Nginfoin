fefe749714a2352de34d3441ae153f28ac10d2f0
Andika Irza Pradana <andikairzapradana@gmail.com>
2021-11-30 22:28:37 +0800

    add button back edit

--- app/src/main/java/com/example/nginfoin/EditActivity.kt
+++ app/src/main/java/com/example/nginfoin/EditActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 class EditActivity : AppCompatActivity() {
@@ -15,6 +16,10 @@ class EditActivity : AppCompatActivity() {
         val btn_edit = include_edit.findViewById<Button>(R.id.button_edit)
         btn_edit.setOnClickListener {
             Toast.makeText(this, "Artikel telah diedit", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, ListActivity::class.java))
+        }
+        val btn_back = findViewById<ImageView>(R.id.back_edit)
+        btn_back.setOnClickListener {
             startActivity(Intent(this, ListActivity::class.java))
         }
     }
