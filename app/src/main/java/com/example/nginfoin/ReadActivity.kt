7651938d836c28588e2fd29e542a2a78424ec715
Kemal Abdillah <60648376+kemal003@users.noreply.github.com>
2021-12-01 07:14:30 +0700

    Update Terakhir

--- app/src/main/java/com/example/nginfoin/ReadActivity.kt
+++ app/src/main/java/com/example/nginfoin/ReadActivity.kt
@@ -6,9 +6,15 @@ import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nginfoin.databinding.ActivityReadBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ReadActivity : AppCompatActivity() {
     private lateinit var binding : ActivityReadBinding
+    private lateinit var databaseRef : DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReadBinding.inflate(layoutInflater)
@@ -17,11 +23,18 @@ class ReadActivity : AppCompatActivity() {
 
         val title = intent.getStringExtra("title")
         val content = intent.getStringExtra("content")
-        val writer = intent.getStringExtra("writer")
+        val writer = intent.getStringExtra("writer").toString()
+        databaseRef = Firebase.database.reference.child("Users").child(writer)
+        println(databaseRef)
+
+        databaseRef.child("nama").get().addOnSuccessListener {
+            val trueWriter = it.value.toString()
+            println(trueWriter)
+            binding.articleWriter.text = "Penulis: $trueWriter"
+        }
 
         binding.articleTitle.text = title
         binding.articleContent.text = content
-        binding.articleWriter.text = "Writer: $writer"
 
         val btn_read = binding.includeAppbarRead.backRead
         btn_read.setOnClickListener{
